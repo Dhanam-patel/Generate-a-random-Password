ad607e372f5039f7d08b740bb5cdd400c8867bdb
Dhanam-patel <166690493+Dhanam-patel@users.noreply.github.com>
2024-09-11 07:16:27 +0530

    Update generate_a_randome_password.c

--- generate_a_randome_password.c
+++ generate_a_randome_password.c
@@ -10,10 +10,10 @@ int main()
     number = (rand()) % (upper - lower + 1) + lower;
   
     srand(time(NULL));
-    /*printf("Enter number of characters you for password: ");
-    scanf("%d", &n);*/  //the commented part won't work in VS CODE if you want to generate a randome password of your wish length then you must use a different compiler or an online compiler here you have to manually change the length of password
+    printf("Enter number of characters you for password: ");
+    scanf("%d", &n);
 
-    for (int i = 1; i <= 5; i++)
+    for (int i = 1; i <= n; i++)
     {
         number = (rand()) % (upper - lower + 1) + lower;
         printf("  %c", number);
